e6e5bfd519968c62988b521848da40719205fb80
senellev <krizz080300@gmail.com>
2024-10-06 00:27:05 +0300

    started the account pages

--- frontend/src/pages/account/Team/Team.tsx
+++ frontend/src/pages/account/Team/Team.tsx
@@ -29,6 +29,69 @@ const Team = () => {
                 </div>
                 
             </li>
+            <li className="team__item">
+                <div className="team__item_left">
+                    <img src="../src/assets/icons/user/user.svg" alt="user" className="team__item__icon" />
+                    <span className="team__item__username">Chrysaint</span>
+                </div>
+                <div className="team__item_right">
+                    <button onClick={() => setModalVisible(modalVisible ? 0 : 1)} className="team__item__actions">...</button>
+                    <ul data-state={`${modalVisible}`} className="team__item__action-list">
+                        <li className="team__item__action-item">
+                            Выгнать из команды
+                        </li>
+                        <li className="team__item__action-item">
+                            Выгнать из команды
+                        </li>
+                        <li className="team__item__action-item">
+                            Выгнать из команды
+                        </li>
+                    </ul>
+                </div>
+                
+            </li>
+            <li className="team__item">
+                <div className="team__item_left">
+                    <img src="../src/assets/icons/user/user.svg" alt="user" className="team__item__icon" />
+                    <span className="team__item__username">Chrysaint</span>
+                </div>
+                <div className="team__item_right">
+                    <button onClick={() => setModalVisible(modalVisible ? 0 : 1)} className="team__item__actions">...</button>
+                    <ul data-state={`${modalVisible}`} className="team__item__action-list">
+                        <li className="team__item__action-item">
+                            Выгнать из команды
+                        </li>
+                        <li className="team__item__action-item">
+                            Выгнать из команды
+                        </li>
+                        <li className="team__item__action-item">
+                            Выгнать из команды
+                        </li>
+                    </ul>
+                </div>
+                
+            </li>
+            <li className="team__item">
+                <div className="team__item_left">
+                    <img src="../src/assets/icons/user/user.svg" alt="user" className="team__item__icon" />
+                    <span className="team__item__username">Chrysaint</span>
+                </div>
+                <div className="team__item_right">
+                    <button onClick={() => setModalVisible(modalVisible ? 0 : 1)} className="team__item__actions">...</button>
+                    <ul data-state={`${modalVisible}`} className="team__item__action-list">
+                        <li className="team__item__action-item">
+                            Выгнать из команды
+                        </li>
+                        <li className="team__item__action-item">
+                            Выгнать из команды
+                        </li>
+                        <li className="team__item__action-item">
+                            Выгнать из команды
+                        </li>
+                    </ul>
+                </div>
+                
+            </li>
         </ul>
     </div>
   )
