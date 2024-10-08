2a3f6012201b48b294f92e352a59b4d5b76c394e
senellev <krizz080300@gmail.com>
2024-10-08 22:08:24 +0300

    account tabs and modal windows for picture uploading

--- frontend/src/pages/account/invitations/Invitations.tsx
+++ frontend/src/pages/account/invitations/Invitations.tsx
@@ -1,12 +1,72 @@
-import React from 'react'
-import './invitations.css'
+import React from "react";
+import "./invitations.css";
 
-type Props = {}
+type Props = {};
 
 const Invitations = (props: Props) => {
   return (
-    <div>Invitations</div>
-  )
-}
+    <section className="invites-block">
+      <ul className="invites__list">
+        <li className="invites__item">
+          <p className="invites__item__name">Something</p>
+          <div className="invites__item__actions">
+            <button data-type="accept" className="invites__item__btn">
+              <img
+                className="invites__item__btn-img"
+                src="../src/assets/icons/menu/check.svg"
+                alt="accept"
+              />
+            </button>
+            <button data-type="decline" className="invites__item__btn">
+              <img
+                className="invites__item__btn-img"
+                src="../src/assets/icons/menu/cross.svg"
+                alt="decline"
+              />
+            </button>
+          </div>
+        </li>
+        <li className="invites__item">
+          <p className="invites__item__name">Something</p>
+          <div className="invites__item__actions">
+            <button data-type="accept" className="invites__item__btn">
+              <img
+                className="invites__item__btn-img"
+                src="../src/assets/icons/menu/check.svg"
+                alt="accept"
+              />
+            </button>
+            <button data-type="decline" className="invites__item__btn">
+              <img
+                className="invites__item__btn-img"
+                src="../src/assets/icons/menu/cross.svg"
+                alt="decline"
+              />
+            </button>
+          </div>
+        </li>
+        <li className="invites__item">
+          <p className="invites__item__name">Something</p>
+          <div className="invites__item__actions">
+            <button data-type="accept" className="invites__item__btn">
+              <img
+                className="invites__item__btn-img"
+                src="../src/assets/icons/menu/check.svg"
+                alt="accept"
+              />
+            </button>
+            <button data-type="decline" className="invites__item__btn">
+              <img
+                className="invites__item__btn-img"
+                src="../src/assets/icons/menu/cross.svg"
+                alt="decline"
+              />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </section>
+  );
+};
 
-export default Invitations
+export default Invitations;
--- frontend/src/pages/account/user/User.tsx
+++ frontend/src/pages/account/user/User.tsx
@@ -1,13 +1,57 @@
-import React from 'react'
-
-type Props = {}
+import { useState } from "react";
+import "./user.css";
+type Props = {};
 
 const User = (props: Props) => {
-  return (
-    <div className='user__wrapper'>
-        asd
-    </div>
-  )
-}
+  const [modalOpened, setModalOpened] = useState(0);
 
-export default User
+  return (
+    <div className="user__wrapper">
+      {modalOpened ? (
+        <div className="modal" onClick={() => setModalOpened(0)}>
+          <div className="modal__wrapper">
+            <button className="modal__button" onClick={() => setModalOpened(0)}>
+              <img src="../src/assets/icons/menu/cross.svg" alt="close" />
+            </button>
+            <label htmlFor="upload-file" className="upload-file-label"></label>
+            <input type="file" id="upload-file" />
+          </div>
+        </div>
+      ) : null}
+      <div className="user__left">
+        <div className="user__img">
+          <img src="../src/assets/avatars/1.jpg" alt="" />
+        </div>
+        <button className="default_btn" onClick={() => setModalOpened(1)}>
+          Изменить аватар
+        </button>
+        <button className="default_btn">Пригласить в команду</button>
+        <input
+          type="file"
+          name=""
+          id=""
+          onChange={(e) => console.log(e.target.value)}
+        />
+      </div>
+      <div className="user__right">
+        <h2 className="user__nickname">Chrysaint</h2>
+        <ul className="user__data">
+          <li className="user__data-row">
+            <span className="user__data__text_bold">Команда:</span>
+            <span className="user__data__text">Нет</span>
+          </li>
+          <li className="user__data-row">
+            <span className="user__data__text_bold">Valorant ID:</span>
+            <span className="user__data__text">Smth#kzh</span>
+          </li>
+          <li className="user__data-row">
+            <span className="user__data__text_bold">Discord:</span>
+            <span className="user__data__text">Splanks</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default User;
